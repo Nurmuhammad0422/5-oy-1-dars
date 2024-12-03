7312115a20dded9c6f536ea43192a4a5f4b0d0b9
Nurmuhammad0422 <bnurmuhammad0422@gmail.com>
2024-12-03 19:35:54 +0500

    project end

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
-let elCountryList = document.querySelector(".list")
+let elCountryList = document.querySelector(".list");
 
 function renderCountry(arr, list) {
-    arr.map(item =>{
-        let elCountryItem = document.createElement("li")
-        let elCountryImg = document.createElement("img")
-        let elcontentWrapper = document.createElement("div")
-        let elCountryName = document.createElement("h2")
-        let elCountryPopulation = document.createElement("p")
-        let elCpital = document.createElement("p")
-        let elwrapper = document.createElement("div")
-        let elCountryLikeImg = document.createElement("button")
-        let elcountrysevedImg = document.createElement("button")
-        let elcountrymoreImg = document.createElement("button")
+  arr.map((item) => {
+    let elCountryItem = document.createElement("li");
+    let elCountryImg = document.createElement("img");
+    let elcontentWrapper = document.createElement("div");
+    let elCountryName = document.createElement("h2");
+    let elCountryPopulation = document.createElement("p");
+    let elCpital = document.createElement("p");
+    let elwrapper = document.createElement("div");
+    let elCountryLikeImg = document.createElement("button");
+    let elcountrysevedImg = document.createElement("button");
+    let elcountrymoreImg = document.createElement("button");
 
-        elCountryItem.className = "w-[267px] bg-slate-200 rounded-md overflow-hidden"
-        
-        elCountryImg.src = item.flag
-        elCountryImg.className = "w-full h-[160px] object-cover"
-        elCountryImg.alt = item.name
+    elCountryItem.className =
+      "w-[267px] bg-slate-200 rounded-md overflow-hidden";
 
-        elcontentWrapper.className = "p-[24px]"
+    elCountryImg.src = item.flag;
+    elCountryImg.className = "w-full h-[160px] object-cover";
+    elCountryImg.alt = item.name;
 
-        elwrapper.className = "w-full h-[40px] flex items-center mt-[10px] justify-between"
+    elcontentWrapper.className = "p-[24px]";
 
-        elCountryLikeImg.innerHTML = `
+    elwrapper.className =
+      "w-full h-[40px] flex items-center mt-[10px] justify-between";
+
+    elCountryLikeImg.innerHTML = `
         <button
           class="w-[40px] h-[40px] rounded-full border-[2px] border-black flex items-center justify-center"
         >
@@ -34,8 +36,8 @@ function renderCountry(arr, list) {
             height="22"
           />
         </button>
-        `
-        elcountrysevedImg.innerHTML = `
+        `;
+    elcountrysevedImg.innerHTML = `
         <button
           class="w-[40px] h-[40px] rounded-full border-[2px] border-black flex items-center justify-center"
         >
@@ -46,9 +48,9 @@ function renderCountry(arr, list) {
             height="22"
           />
         </button>
-        `
+        `;
 
-        elcountrymoreImg.innerHTML = `
+    elcountrymoreImg.innerHTML = `
         <button
           class="w-[40px] h-[40px] rounded-full border-[2px] border-black flex items-center justify-center"
         >
@@ -59,19 +61,23 @@ function renderCountry(arr, list) {
             height="22"
           />
         </button>
-        `
+        `;
 
-        elCountryName.textContent = item.name
-        elCountryName.className = "text-[22px] font-bold"
+    elCountryName.textContent = item.name;
+    elCountryName.className = "text-[22px] font-bold";
 
-        elCountryPopulation.textContent = `Population: ${item.population}`
-        elCpital.textContent = `Capital: ${item.capital}`
+    elCountryPopulation.textContent = `Population: ${item.population}`;
+    elCpital.textContent = `Capital: ${item.capital}`;
 
-        
-        elCountryItem.append(elCountryImg, elcontentWrapper)
-        elwrapper.append(elCountryLikeImg, elcountrysevedImg, elcountrymoreImg)
-        elcontentWrapper.append(elCountryName, elCountryPopulation, elCpital, elwrapper)
-        list.append(elCountryItem)
-    })
+    elCountryItem.append(elCountryImg, elcontentWrapper);
+    elwrapper.append(elCountryLikeImg, elcountrysevedImg, elcountrymoreImg);
+    elcontentWrapper.append(
+      elCountryName,
+      elCountryPopulation,
+      elCpital,
+      elwrapper
+    );
+    list.append(elCountryItem);
+  });
 }
-renderCountry(countrys, elCountryList)
+renderCountry(countrys, elCountryList);
